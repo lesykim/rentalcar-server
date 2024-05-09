8ab657846e7445a4fd600fe277a49b753cfbacae
lesykim <trchol01@naver.com>
2024-05-09 18:37:23 +0900

    Update User

--- src/main/java/rentalServer/user/model/User.java
+++ src/main/java/rentalServer/user/model/User.java
@@ -1,5 +1,47 @@
 package rentalServer.user.model;
 
-public class User {
+import java.sql.Timestamp;
 
+public class User {
+	private String id;
+	private String name;
+	private String birth;
+	private String phone;
+	private String licenseDate;
+	private Timestamp hireDate;
+	
+	public User(String id, String name, String birth, String phone, String licenseDate, Timestamp hireDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birth = birth;
+		this.phone = phone;
+		this.licenseDate = licenseDate;
+		this.hireDate = hireDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getLicenseDate() {
+		return licenseDate;
+	}
+
+	public Timestamp getHireDate() {
+		return hireDate;
+	}
+	
 }
