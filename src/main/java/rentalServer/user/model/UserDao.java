1793a51dc1895a4c7b92999cfbd30b61942059bb
lesykim <trchol01@naver.com>
2024-05-09 18:40:11 +0900

    UserDao change to instance

--- src/main/java/rentalServer/user/model/UserDao.java
+++ src/main/java/rentalServer/user/model/UserDao.java
@@ -1,5 +1,23 @@
 package rentalServer.user.model;
 
-public class UserDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class UserDao {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private UserDao() {
+		
+	}
+	
+	private static UserDao instance = new UserDao();
+	
+	public static UserDao getInstance() {
+		return instance;
+	}
+	
 }
